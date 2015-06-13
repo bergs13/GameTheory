bc0f9c355348da18bad240e2f341184fd8f2ae38
bergs13 <bergs13@bfh.ch>
2015-06-13 05:33:14 +0200

    removed some warnings

--- GameTheory/src/defs/dame/DameGameState.java
+++ GameTheory/src/defs/dame/DameGameState.java
@@ -15,83 +15,86 @@ import defs.general.GenericRow;
  */
 public class DameGameState extends GameState {
 
-    private DameTable dameTable = null;
+	private DameTable dameTable = null;
 
-    public DameGameState() {
-    }
+	public DameGameState() {
+	}
 
-    public DameGameState(GameState parentState, Move move, DameTable dameTable) {
-        super(parentState, move);
-        this.dameTable = dameTable;
-        this.dameTable.moveValue(((DameMove)move).getMovement()[0], ((DameMove)move).getMovement()[1], 
-                ((DameMove)move).getMovement()[2], ((DameMove)move).getMovement()[3], Piece.EMPTY);
-        super.setPlayerToMove(parentState.getPlayerToWait());
-        super.setPlayerToWait(parentState.getPlayerToMove());
-    }
+	public DameGameState(GameState parentState, Move move, DameTable dameTable) {
+		super(parentState, move);
+		this.dameTable = dameTable;
+		this.dameTable.moveValue(((DameMove) move).getMovement()[0],
+				((DameMove) move).getMovement()[1],
+				((DameMove) move).getMovement()[2],
+				((DameMove) move).getMovement()[3], Piece.EMPTY);
+		super.setPlayerToMove(parentState.getPlayerToWait());
+		super.setPlayerToWait(parentState.getPlayerToMove());
+	}
 
-    // Methods
-    // Base overrides
-    @Override
-    public void findPossibleMoves() {
-        setChildMoves(this.dameTable
-                .getAllPossibleMoves(((DamePlayer) getPlayerToMove())
-                        .getPlayersPiece()));
-    }
+	// Methods
+	// Base overrides
+	@Override
+	public void findPossibleMoves() {
+		setChildMoves(this.dameTable
+				.getAllPossibleMoves(((DamePlayer) getPlayerToMove())
+						.getPlayersPiece()));
+	}
 
-    @Override
-    public void createChildStates() {
-        super.createChildStates();
-        List<GameState> childStates = getChildStates();
-        for (Move move : getAllMoves()) {
-            childStates.add(new DameGameState(this, move, new DameTable(this.dameTable)));
-        }
-    }
+	@Override
+	public void createChildStates() {
+		super.createChildStates();
+		List<GameState> childStates = getChildStates();
+		for (Move move : getAllMoves()) {
+			childStates.add(new DameGameState(this, move, new DameTable(
+					this.dameTable)));
+		}
+	}
 
-    @Override
-    public boolean isTerminal() {
-        return (getAllMoves().isEmpty());
-    }
+	@Override
+	public boolean isTerminal() {
+		return (getAllMoves().isEmpty());
+	}
 
-    @Override
-    public DameGameState doMove(Move move) {
-        return new DameGameState(this, move, this.dameTable);
-        
-    }
+	@Override
+	public DameGameState doMove(Move move) {
+		return new DameGameState(this, move, this.dameTable);
 
-    public void setStartState(Player firstPlayer, Player secondPlayer,
-            DameTable dameTable) {
-        super.setStartState(firstPlayer, secondPlayer);
-        this.dameTable = dameTable;
-    }
+	}
 
-    public DameTable getDameTable() {
-        return this.dameTable;
-    }
+	public void setStartState(Player firstPlayer, Player secondPlayer,
+			DameTable dameTable) {
+		super.setStartState(firstPlayer, secondPlayer);
+		this.dameTable = dameTable;
+	}
 
-    public String allPossibleMovesToString() {
-        String allMoves = "";
-        for (Move move : super.getChildMoves()) {
-            String moveString = "from " + ((DameMove) move).getMovement()[0]
-                    + " , " + ((DameMove) move).getMovement()[1]
-                    + " to " + ((DameMove) move).getMovement()[2]
-                    + " , " + ((DameMove) move).getMovement()[3] 
-                    + " : " + ((DameMove) move).canCapture() + "\n";
-            allMoves = allMoves.concat(moveString);
-        }
-        return allMoves;
-    }
+	public DameTable getDameTable() {
+		return this.dameTable;
+	}
 
-    public String tableToString() {
-        String tableString = "";
-        for (GenericRow<DameConstants.Piece> row : getDameTable().getRows()) {
-            String rowString = "";
-            for (GenericCell cell : row.getCells()) {
-                Piece piece = (Piece)cell.getCellValue();
-                rowString = rowString.concat(" " +piece);
-            }
-            tableString = tableString.concat(rowString + "\n");
-        }
-        return tableString;
-    }
+	public String allPossibleMovesToString() {
+		String allMoves = "";
+		for (Move move : super.getChildMoves()) {
+			String moveString = "from " + ((DameMove) move).getMovement()[0]
+					+ " , " + ((DameMove) move).getMovement()[1] + " to "
+					+ ((DameMove) move).getMovement()[2] + " , "
+					+ ((DameMove) move).getMovement()[3] + " : "
+					+ ((DameMove) move).canCapture() + "\n";
+			allMoves = allMoves.concat(moveString);
+		}
+		return allMoves;
+	}
+
+	public String tableToString() {
+		String tableString = "";
+		for (GenericRow<DameConstants.Piece> row : getDameTable().getRows()) {
+			String rowString = "";
+			for (GenericCell<Piece> cell : row.getCells()) {
+				Piece piece = (Piece) cell.getCellValue();
+				rowString = rowString.concat(" " + piece);
+			}
+			tableString = tableString.concat(rowString + "\n");
+		}
+		return tableString;
+	}
 
 }
--- GameTheory/src/defs/general/Algorithms.java
+++ GameTheory/src/defs/general/Algorithms.java
@@ -1,66 +1,67 @@
 package defs.general;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /*
  * Klasse f�r die Algorithmen (Als statische Methoden)
  */
 public class Algorithms {
 
-    // Methoden
-    // Statische Methoden
-    public static int alphaBeta(Evaluator evaluator, GameState gstate, int depth, int alpha, int beta) {
-        int bestValue = Integer.MIN_VALUE;
-        if (gstate.isTerminal() || depth == 0) {
-            return evaluator.evaluate(gstate);
-        } else {
-            for (GameState childState : gstate.getChildStates()) {
-                int value = -alphaBeta(evaluator, childState, depth - 1, -beta, -alpha);
-                bestValue = Math.max(bestValue, value);
-                alpha = Math.max(value, alpha);
-                if (alpha >= beta) {
-                    break;
-                }
-            }
-        }
-        return bestValue;
-    }
+	// Methoden
+	// Statische Methoden
+	public static int alphaBeta(Evaluator evaluator, GameState gstate,
+			int depth, int alpha, int beta) {
+		int bestValue = Integer.MIN_VALUE;
+		if (gstate.isTerminal() || depth == 0) {
+			return evaluator.evaluate(gstate);
+		} else {
+			for (GameState childState : gstate.getChildStates()) {
+				int value = -alphaBeta(evaluator, childState, depth - 1, -beta,
+						-alpha);
+				bestValue = Math.max(bestValue, value);
+				alpha = Math.max(value, alpha);
+				if (alpha >= beta) {
+					break;
+				}
+			}
+		}
+		return bestValue;
+	}
 
-    public static int miniMax(Evaluator evaluator, GameState gstate, int depth) {
-        int bestValue = Integer.MIN_VALUE;
-        if (gstate.isTerminal() || depth == 0) {
-            return evaluator.evaluate(gstate);
-        } else {
-            for (GameState childState : gstate.getChildStates()) {
-                bestValue = Math.max(bestValue, -miniMax(evaluator, childState, depth));
-            }
-        }
-        return bestValue;
-    }
+	public static int miniMax(Evaluator evaluator, GameState gstate, int depth) {
+		int bestValue = Integer.MIN_VALUE;
+		if (gstate.isTerminal() || depth == 0) {
+			return evaluator.evaluate(gstate);
+		} else {
+			for (GameState childState : gstate.getChildStates()) {
+				bestValue = Math.max(bestValue,
+						-miniMax(evaluator, childState, depth));
+			}
+		}
+		return bestValue;
+	}
 
-    //
+	//
 
-//    public static int alphaBeta2(Evaluator evaluator, GameState gstate, int depth, int alpha, int beta) {
-//        GameState bestGameState = new GameState();
-//        bestGameState.setValue(Integer.MIN_VALUE);
-//        if (gstate.isTerminal() || depth == 0) {
-//            gstate.setValue(evaluator.evaluate(gstate));
-//            return gstate.getValue();
-//        } else {
-//            for (GameState childState : gstate.getChildStates()) {
-//                int value= -alphaBeta(evaluator, childState, depth - 1, -beta, -alpha);
-//                bestGameState.setValue(Math.max(bestGameState.getValue(), value));
-//                
-//                }
-//                alpha = Math.max(childState.getValue(), alpha);
-//                if (alpha >= beta) {
-//                    break;
-//                }
-//            }
-//        }
-//        System.out.println(bestGameState.getValue());
-//        return bestGameState.getValue();
-//    }
+	// public static int alphaBeta2(Evaluator evaluator, GameState gstate, int
+	// depth, int alpha, int beta) {
+	// GameState bestGameState = new GameState();
+	// bestGameState.setValue(Integer.MIN_VALUE);
+	// if (gstate.isTerminal() || depth == 0) {
+	// gstate.setValue(evaluator.evaluate(gstate));
+	// return gstate.getValue();
+	// } else {
+	// for (GameState childState : gstate.getChildStates()) {
+	// int value= -alphaBeta(evaluator, childState, depth - 1, -beta, -alpha);
+	// bestGameState.setValue(Math.max(bestGameState.getValue(), value));
+	//
+	// }
+	// alpha = Math.max(childState.getValue(), alpha);
+	// if (alpha >= beta) {
+	// break;
+	// }
+	// }
+	// }
+	// System.out.println(bestGameState.getValue());
+	// return bestGameState.getValue();
+	// }
 
 }
